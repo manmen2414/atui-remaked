fbc1f1fe3526b02d673ad5c7835b80faff8d429d
manmen2414 <inotitokokoro@gmail.com>
2026-06-22 17:28:30 +0900

    Move README readmeを移動し、help時の表示を変更

--- src/atui/functions/help.ts
+++ src/atui/functions/help.ts
@@ -1,9 +1,10 @@
 import { Atui } from "..";
 import { AtuiResponseBuilder } from "../ResponseBuilder";
 import { AtuiBaseFunction, HandlerResult } from "./AtuiFunction";
+import readme from "../../../README.md?raw";
 
 export class HelpFunction extends AtuiBaseFunction {
-  description: string = `Atuiの説明を表示します。現在非推奨です。`;
+  description: string = `Atuiの説明を表示します。`;
   constructor() {
     super("help");
   }
@@ -14,15 +15,10 @@ export class HelpFunction extends AtuiBaseFunction {
   ): Promise<HandlerResult> {
     const content = resBuilder.req.content;
     if (content.includes("help") || content.includes("ヘルプ")) {
-      await atui._emitRes(resBuilder.md("説明書を取得しています。"), 200);
-      const readme = await fetch("README.md")
-        .then((r) => r.text())
-        .catch((ex) => {
-          atui._emitRes(resBuilder.error(`エラーが発生しました。\n${ex}`));
-          return "";
-        });
       if (!!readme) {
         await atui._emitRes(resBuilder.md(readme));
+      } else {
+        await atui._emitRes(resBuilder.md("説明書データがありません。"));
       }
       return { handleNext: false, changeMode: false };
     }
